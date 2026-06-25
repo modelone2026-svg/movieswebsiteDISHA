4c63888b005a832ebf95994482d1df2edd9ba303
modelone2026-svg <modelone2026@gmail.com>
2026-06-26 00:06:15 +0300

    Add files via upload

--- base.py
+++ base.py
@@ -1,208 +1,290 @@
-from urllib.parse import unquote, urlencode, urlsplit, urlunsplit
+import logging
+from urllib.parse import urlparse
 
-from asgiref.local import Local
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
 
-from django.http import QueryDict
-from django.utils.functional import lazy
-from django.utils.translation import override
+from django.core.exceptions import ImproperlyConfigured
+from django.http import (
+    HttpResponse,
+    HttpResponseGone,
+    HttpResponseNotAllowed,
+    HttpResponsePermanentRedirect,
+    HttpResponseRedirect,
+)
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.decorators import classonlymethod
+from django.utils.functional import classproperty
+from django.utils.log import log_response
 
-from .exceptions import NoReverseMatch, Resolver404
-from .resolvers import _get_cached_resolver, get_ns_resolver, get_resolver
-from .utils import get_callable
-
-# SCRIPT_NAME prefixes for each thread are stored here. If there's no entry for
-# the current thread (which is the only one we ever access), it is assumed to
-# be empty.
-_prefixes = Local()
-
-# Overridden URLconfs for each thread are stored here.
-_urlconfs = Local()
-
-
-def resolve(path, urlconf=None):
-    if urlconf is None:
-        urlconf = get_urlconf()
-    return get_resolver(urlconf).resolve(path)
+logger = logging.getLogger("django.request")
 
 
-def reverse(
-    viewname,
-    urlconf=None,
-    args=None,
-    kwargs=None,
-    current_app=None,
-    *,
-    query=None,
-    fragment=None,
-):
-    if urlconf is None:
-        urlconf = get_urlconf()
-    resolver = get_resolver(urlconf)
-    args = args or []
-    kwargs = kwargs or {}
+class ContextMixin:
+    """
+    A default context mixin that passes the keyword arguments received by
+    get_context_data() as the template context.
+    """
 
-    prefix = get_script_prefix()
+    extra_context = None
 
-    if not isinstance(viewname, str):
-        view = viewname
-    else:
-        *path, view = viewname.split(":")
+    def get_context_data(self, **kwargs):
+        kwargs.setdefault("view", self)
+        if self.extra_context is not None:
+            kwargs.update(self.extra_context)
+        return kwargs
 
-        if current_app:
-            current_path = current_app.split(":")
-            current_path.reverse()
-        else:
-            current_path = None
 
-        resolved_path = []
-        ns_pattern = ""
-        ns_converters = {}
-        for ns in path:
-            current_ns = current_path.pop() if current_path else None
-            # Lookup the name to see if it could be an app identifier.
-            try:
-                app_list = resolver.app_dict[ns]
-                # Yes! Path part matches an app in the current Resolver.
-                if current_ns and current_ns in app_list:
-                    # If we are reversing for a particular app, use that
-                    # namespace.
-                    ns = current_ns
-                elif ns not in app_list:
-                    # The name isn't shared by one of the instances (i.e.,
-                    # the default) so pick the first instance as the default.
-                    ns = app_list[0]
-            except KeyError:
-                pass
+class View:
+    """
+    Intentionally simple parent class for all views. Only implements
+    dispatch-by-method and simple sanity checking.
+    """
 
-            if ns != current_ns:
-                current_path = None
+    http_method_names = [
+        "get",
+        "post",
+        "put",
+        "patch",
+        "delete",
+        "head",
+        "options",
+        "trace",
+    ]
 
-            try:
-                extra, resolver = resolver.namespace_dict[ns]
-                resolved_path.append(ns)
-                ns_pattern += extra
-                ns_converters.update(resolver.pattern.converters)
-            except KeyError as key:
-                if resolved_path:
-                    raise NoReverseMatch(
-                        "%s is not a registered namespace inside '%s'"
-                        % (key, ":".join(resolved_path))
-                    )
-                else:
-                    raise NoReverseMatch("%s is not a registered namespace" % key)
-        if ns_pattern:
-            resolver = get_ns_resolver(
-                ns_pattern, resolver, tuple(ns_converters.items())
+    def __init__(self, **kwargs):
+        """
+        Constructor. Called in the URLconf; can contain helpful extra
+        keyword arguments, and other things.
+        """
+        # Go through keyword arguments, and either save their values to our
+        # instance, or raise an error.
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classproperty
+    def view_is_async(cls):
+        handlers = [
+            getattr(cls, method)
+            for method in cls.http_method_names
+            if (method != "options" and hasattr(cls, method))
+        ]
+        if not handlers:
+            return False
+        is_async = iscoroutinefunction(handlers[0])
+        if not all(iscoroutinefunction(h) == is_async for h in handlers[1:]):
+            raise ImproperlyConfigured(
+                f"{cls.__qualname__} HTTP handlers must either be all sync or all "
+                "async."
             )
+        return is_async
 
-    resolved_url = resolver._reverse_with_prefix(view, prefix, *args, **kwargs)
-    if query is not None:
-        if isinstance(query, QueryDict):
-            query_string = query.urlencode()
-        else:
-            query_string = urlencode(query, doseq=True)
-        if query_string:
-            resolved_url += "?" + query_string
-    if fragment is not None:
-        resolved_url += "#" + fragment
-    return resolved_url
-
-
-reverse_lazy = lazy(reverse, str)
-
-
-def clear_url_caches():
-    get_callable.cache_clear()
-    _get_cached_resolver.cache_clear()
-    get_ns_resolver.cache_clear()
-
-
-def set_script_prefix(prefix):
-    """
-    Set the script prefix for the current thread.
-    """
-    if not prefix.endswith("/"):
-        prefix += "/"
-    _prefixes.value = prefix
-
-
-def get_script_prefix():
-    """
-    Return the currently active script prefix. Useful for client code that
-    wishes to construct their own URLs manually (although accessing the request
-    instance is normally going to be a lot cleaner).
-    """
-    return getattr(_prefixes, "value", "/")
-
-
-def clear_script_prefix():
-    """
-    Unset the script prefix for the current thread.
-    """
-    try:
-        del _prefixes.value
-    except AttributeError:
-        pass
-
-
-def set_urlconf(urlconf_name):
-    """
-    Set the URLconf for the current thread or asyncio task (overriding the
-    default one in settings). If urlconf_name is None, revert back to the
-    default.
-    """
-    if urlconf_name:
-        _urlconfs.value = urlconf_name
-    else:
-        if hasattr(_urlconfs, "value"):
-            del _urlconfs.value
-
-
-def get_urlconf(default=None):
-    """
-    Return the root URLconf to use for the current thread or asyncio task if it
-    has been changed from the default one.
-    """
-    return getattr(_urlconfs, "value", default)
-
-
-def is_valid_path(path, urlconf=None):
-    """
-    Return the ResolverMatch if the given path resolves against the default URL
-    resolver, False otherwise. This is a convenience method to make working
-    with "is this a match?" cases easier, avoiding try...except blocks.
-    """
-    try:
-        return resolve(path, urlconf)
-    except Resolver404:
-        return False
-
-
-def translate_url(url, lang_code):
-    """
-    Given a URL (absolute or relative), try to get its translated version in
-    the `lang_code` language (either by i18n_patterns or by translated regex).
-    Return the original URL if no translated version is found.
-    """
-    parsed = urlsplit(url)
-    try:
-        # URL may be encoded.
-        match = resolve(unquote(parsed.path))
-    except Resolver404:
-        pass
-    else:
-        to_be_reversed = (
-            "%s:%s" % (match.namespace, match.url_name)
-            if match.namespace
-            else match.url_name
-        )
-        with override(lang_code):
-            try:
-                url = reverse(to_be_reversed, args=match.args, kwargs=match.kwargs)
-            except NoReverseMatch:
-                pass
-            else:
-                url = urlunsplit(
-                    (parsed.scheme, parsed.netloc, url, parsed.query, parsed.fragment)
+    @classonlymethod
+    def as_view(cls, **initkwargs):
+        """Main entry point for a request-response process."""
+        for key in initkwargs:
+            if key in cls.http_method_names:
+                raise TypeError(
+                    "The method name %s is not accepted as a keyword argument "
+                    "to %s()." % (key, cls.__name__)
                 )
-    return url
+            if not hasattr(cls, key):
+                raise TypeError(
+                    "%s() received an invalid keyword %r. as_view "
+                    "only accepts arguments that are already "
+                    "attributes of the class." % (cls.__name__, key)
+                )
+
+        def view(request, *args, **kwargs):
+            self = cls(**initkwargs)
+            self.setup(request, *args, **kwargs)
+            if not hasattr(self, "request"):
+                raise AttributeError(
+                    "%s instance has no 'request' attribute. Did you override "
+                    "setup() and forget to call super()?" % cls.__name__
+                )
+            return self.dispatch(request, *args, **kwargs)
+
+        view.view_class = cls
+        view.view_initkwargs = initkwargs
+
+        # __name__ and __qualname__ are intentionally left unchanged as
+        # view_class should be used to robustly determine the name of the view
+        # instead.
+        view.__doc__ = cls.__doc__
+        view.__module__ = cls.__module__
+        view.__annotations__ = cls.dispatch.__annotations__
+        # Copy possible attributes set by decorators, e.g. @csrf_exempt, from
+        # the dispatch method.
+        view.__dict__.update(cls.dispatch.__dict__)
+
+        # Mark the callback if the view class is async.
+        if cls.view_is_async:
+            markcoroutinefunction(view)
+
+        return view
+
+    def setup(self, request, *args, **kwargs):
+        """Initialize attributes shared by all view methods."""
+        if hasattr(self, "get") and not hasattr(self, "head"):
+            self.head = self.get
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+
+    def dispatch(self, request, *args, **kwargs):
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(
+                self, request.method.lower(), self.http_method_not_allowed
+            )
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        response = HttpResponseNotAllowed(self._allowed_methods())
+        log_response(
+            "Method Not Allowed (%s): %s",
+            request.method,
+            request.path,
+            response=response,
+            request=request,
+        )
+
+        if self.view_is_async:
+
+            async def func():
+                return response
+
+            return func()
+        else:
+            return response
+
+    def options(self, request, *args, **kwargs):
+        """Handle responding to requests for the OPTIONS HTTP verb."""
+        response = HttpResponse()
+        response.headers["Allow"] = ", ".join(self._allowed_methods())
+        response.headers["Content-Length"] = "0"
+
+        if self.view_is_async:
+
+            async def func():
+                return response
+
+            return func()
+        else:
+            return response
+
+    def _allowed_methods(self):
+        return [m.upper() for m in self.http_method_names if hasattr(self, m)]
+
+
+class TemplateResponseMixin:
+    """A mixin that can be used to render a template."""
+
+    template_name = None
+    template_engine = None
+    response_class = TemplateResponse
+    content_type = None
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Return a response, using the `response_class` for this view, with a
+        template rendered with the given context.
+
+        Pass response_kwargs to the constructor of the response class.
+        """
+        response_kwargs.setdefault("content_type", self.content_type)
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs,
+        )
+
+    def get_template_names(self):
+        """
+        Return a list of template names to be used for the request. Must return
+        a list. May not be called if render_to_response() is overridden.
+        """
+        if self.template_name is None:
+            raise ImproperlyConfigured(
+                "TemplateResponseMixin requires either a definition of "
+                "'template_name' or an implementation of 'get_template_names()'"
+            )
+        else:
+            return [self.template_name]
+
+
+class TemplateView(TemplateResponseMixin, ContextMixin, View):
+    """
+    Render a template. Pass keyword arguments from the URLconf to the context.
+    """
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+
+class RedirectView(View):
+    """Provide a redirect on any GET request."""
+
+    permanent = False
+    url = None
+    pattern_name = None
+    query_string = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        """
+        Return the URL redirect to. Keyword arguments from the URL pattern
+        match generating the redirect request are provided as kwargs to this
+        method.
+        """
+        if self.url:
+            url = self.url % kwargs
+        elif self.pattern_name:
+            url = reverse(self.pattern_name, args=args, kwargs=kwargs)
+        else:
+            return None
+
+        args = self.request.META.get("QUERY_STRING", "")
+        if args and self.query_string:
+            if urlparse(url).query:
+                url = f"{url}&{args}"
+            else:
+                url = f"{url}?{args}"
+        return url
+
+    def get(self, request, *args, **kwargs):
+        url = self.get_redirect_url(*args, **kwargs)
+        if url:
+            if self.permanent:
+                return HttpResponsePermanentRedirect(url)
+            else:
+                return HttpResponseRedirect(url)
+        else:
+            response = HttpResponseGone()
+            log_response("Gone: %s", request.path, response=response, request=request)
+            return response
+
+    def head(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def options(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
